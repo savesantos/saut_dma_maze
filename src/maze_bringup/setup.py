0517e7298555430325011754d94c20edafd1a705
Salvador Santos <salvadorvelososantos@gmail.com>
2026-05-03 14:34:09 +0100

    Initialize ROS 2 colcon workspace with maze_mdp, maze_bringup, maze_msgs
    - Create src/ layout following the official ROS 2 Humble convention
    - Add maze_mdp (ament_python): MDP, VI, SARSA, Q-Learning, micro-simulator
    - Add maze_bringup (ament_python): launch files & config for AlphaBot2
    - Add maze_msgs (ament_cmake): custom ROS 2 interfaces
    - Ignore colcon build/install/log directories
    - Document setup and dev workflow in README.md and docs/tutorial.md
    - Include course materials (PDFs)

---
+++ src/maze_bringup/setup.py
@@ -0,0 +1,29 @@
+from setuptools import find_packages, setup
+
+package_name = 'maze_bringup'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='Salvador Santos',
+    maintainer_email='salvadorvelososantos@gmail.com',
+    description='Launch files and configuration for the AlphaBot2 maze-solving stack.',
+    license='Apache-2.0',
+    extras_require={
+        'test': [
+            'pytest',
+        ],
+    },
+    entry_points={
+        'console_scripts': [
+        ],
+    },
+)
